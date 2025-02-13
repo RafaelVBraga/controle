60fedc2f84a102151294d2677f62b3a0c4c98ed6
Rafael <rafael.vieira.braga@gmail.com>
2025-02-13 15:49:56 -0300

    Atualização do Markdown

--- src/main/java/br/gov/caucaia/sme/apps/controleinterno/controllers/MarkDownController.java
+++ src/main/java/br/gov/caucaia/sme/apps/controleinterno/controllers/MarkDownController.java
@@ -1,41 +1,61 @@
 package br.gov.caucaia.sme.apps.controleinterno.controllers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@Controller
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
+
+@Controller@RequestMapping("/markdown")
 public class MarkDownController {
 
-    @Autowired
-    private Resource markdownFile;
+	 @Value("${markDown.file.path}")
+	    private String markdownFile;
 
-    @GetMapping("/edit")
-    public String editMarkdown(Model model) {
-        try {
-            String content = new String(Files.readAllBytes(Paths.get(markdownFile.getURL().getPath())));
-            model.addAttribute("content", content);
-        } catch (Exception e) {
-            model.addAttribute("error", "Erro ao carregar o arquivo Markdown.");
-        }
-        return "editMarkdown";
-    }
+	    private final ResourceLoader resourceLoader;
+	
+	    public MarkDownController(ResourceLoader resourceLoader) {
+	        this.resourceLoader = resourceLoader;
+	    }
+	    @Data
+	    public class Teste {
+	    	String valor;
+	    }
 
-    @PostMapping("/save")
-    public String saveMarkdown(@RequestParam("content") String content, Model model) {
-        try {
-            Files.write(Paths.get(markdownFile.getURL().getPath()), content.getBytes());
-            model.addAttribute("message", "Arquivo salvo com sucesso!");
-        } catch (IOException e) {
-            model.addAttribute("error", "Erro ao salvar o arquivo Markdown.");
-        }
-        return "editMarkdown";
-    }
+	    @GetMapping("/edit")
+	    public String editMarkdown(Model model) {
+	        try {
+	        	Teste teste = new Teste();
+	            Resource resource = resourceLoader.getResource(markdownFile);
+	           teste.setValor(new String(Files.readAllBytes(Paths.get(resource.getURI()))));
+	         
+	            model.addAttribute("teste", teste);
+	        } catch (Exception e) {
+	            model.addAttribute("error", "Erro ao carregar o arquivo Markdown.");
+	        }
+	        return "/util/editMarkdown.xhtml";
+	    }
+
+	    @PostMapping("/save")
+	    public String saveMarkdown(Model model, @ModelAttribute Teste teste) {
+	        try {
+	        	System.out.println(teste.valor);
+	            Resource resource = resourceLoader.getResource(markdownFile);
+	            Files.write(Paths.get(resource.getURI()), teste.getValor().getBytes());
+	            model.addAttribute("message", "Arquivo salvo com sucesso!");
+	        } catch (IOException e) {
+	            model.addAttribute("error", "Erro ao salvar o arquivo Markdown.");
+	        }
+	        
+	        return "redirect:/home.xhtml";
+	    }
 }
